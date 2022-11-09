dc5459ba343df196b26f50457ee53b03a66537ee
leela khadka <khadkaleela3@gmail.com>
2022-11-09 15:11:55 -0800

    remove assosations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :bookings, dependent: :destroy
     has_many :dance_classes, through: :bookings
-    has_many :reviews, through: :dance_classes
+    has_many :reviews
 
     validates :email, presence: true, uniqueness: true
 end
